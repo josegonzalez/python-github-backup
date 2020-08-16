cb1b0b6c6bb3fb2f982f775ee831abf20916c020
Albert Wang <git@albertyw.com>
2020-08-15 17:35:39 -0700

    Add support for python 3.7 and 3.8 in package classifiers

--- setup.py
+++ setup.py
@@ -25,6 +25,8 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
     description='backup a github user or organization',
     long_description=open_file('README.rst').read(),
