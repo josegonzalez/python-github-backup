d7f07474326610bd6a01ac63157125b0ac43d450
Albert Wang <git@albertyw.com>
2020-08-15 17:35:18 -0700

    Remove support for python 2.7 in package classifiers

--- setup.py
+++ setup.py
@@ -23,7 +23,6 @@ setup(
         'Development Status :: 5 - Production/Stable',
         'Topic :: System :: Archiving :: Backup',
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
     ],
